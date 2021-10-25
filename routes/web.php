a9b7a492d9e9715fb2630e52dbc72c2b43013c36
Joana Paula <joanapaulasoliveira@gmail.com>
2021-10-25 19:14:25 -0300

    Route correction

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers;
+use App\Http\Controllers\ContatoController;
 
 /*
 |--------------------------------------------------------------------------
@@ -18,4 +18,4 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::post('contato', [App\Http\Controllers\ContatoController::class, 'send'])->name('contato');
+Route::post('/contato', [ContatoController::class, 'send']);
