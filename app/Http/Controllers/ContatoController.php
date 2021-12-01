80d7790c3fd3c60d3b77c17879fb1e8a9829ba79
joanapaulasoliveira <joanapaulasoliveira@gmail.com>
2021-12-01 13:24:37 -0300

    ReUploading 3

--- app/Http/Controllers/ContatoController.php
+++ app/Http/Controllers/ContatoController.php
@@ -16,7 +16,7 @@ class ContatoController extends Controller
             'msg' => $request->msg
         );
 
-        Mail::to('scarlet.sindra@trendom.online')->send(new Contato($data));
+        Mail::to('scarlet.sindra@trendom.com.br')->send(new Contato($data));
         return redirect('/#enviado')->with('success', 'Agradecemos o seu contato!');
     }
 }
